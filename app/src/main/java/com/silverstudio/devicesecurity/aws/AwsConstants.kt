368f8a297cf6de9e140f94ce87c3e49045763d39
advait <advaitr60@gmail.com>
2022-05-29 16:07:55 +0530

    Removed api ids

--- app/src/main/java/com/silverstudio/devicesecurity/aws/AwsConstants.kt
+++ app/src/main/java/com/silverstudio/devicesecurity/aws/AwsConstants.kt
@@ -1,10 +1,13 @@
 package com.silverstudio.devicesecurity.aws
 
+import android.content.res.Resources
 import com.amazonaws.regions.Regions
+import com.silverstudio.devicesecurity.R
 
 object AwsConstants {
 
-    val COGNITO_IDENTITY_ID: String = "ap-south-1:7e09adee-565c-41c6-a44c-92a67068ff28"
+    val COGNITO_IDENTITY_ID: String = Resources.getSystem().getString(R.string.cognito_id)
+
     val COGNITO_REGION: Regions = Regions.AP_SOUTH_1 // Region
     val BUCKET_NAME: String = "bucket-customer-images"
     val S3_URL: String = "https://$BUCKET_NAME.s3.ap-south-1.amazonaws.com/"
